7fe0d7d7b76c4bbbe2b49d9db7c8c3abdc45d076
Aviv Levinsky <levinskyaviv@gmail.com>
2026-03-30 00:46:35 +0300

    clean up main.py

--- bwsa_beta_supply/main.py
+++ bwsa_beta_supply/main.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python3
 
-import numpy as np
 import json
-from scipy.stats import linregress
 
 import pandas as pd
 import plotly.graph_objects as go
-import plotly.express as px
 
 
 import os
@@ -44,28 +41,13 @@ def main():
     ) - df.where(df.lt(df.quantile(0.1))).stack().groupby(level=1).agg('mean')
     beta_dispersion_trace = beta_dispersion.to_frame(name='beta_dispersion').plot()
 
-    beta_supply['short_slope'] = beta_supply.rolling(7).apply(
-        lambda s: linregress(range(len(s)), s)[0]
-    )
-    slope_deriv = np.gradient(beta_supply['short_slope'])
-    infls = np.where(np.diff(np.sign(slope_deriv)))[0]
-
-    short_trace = go.Figure(
-        data=go.Scatter(
-            x=beta_supply.index,
-            y=beta_supply['short_slope'].values,
-            name='7-day change',
-        )
-    )
-
     figs = (
         go.Figure(
-            data=short_trace.data
-            + beta_supply['supply_count'].plot().data
+            data=beta_supply['supply_count'].plot().data
             + beta_dispersion_trace.data
         )
         .update_layout(
-            title=dict(text='Beta supply', font_size=24, x=0.5),
+            title=dict(text='Beta Supply', font_size=24, x=0.5),
             xaxis=dict(
                 ticklabelmode='period',
                 dtick='M1',
@@ -87,35 +69,14 @@ def main():
             yaxis=dict(showline=True, showgrid=False),
             template='plotly_dark',
             legend=dict(x=0.1, y=1.1, orientation='h', font=dict(color='#FFFFFF')),
-            yaxis_title=dict(
-                text='high beta supply rate', font=dict(size=16, color='#FFFFFF')
-            ),
-        )
-        .add_hline(
-            y=-1 * beta_supply['short_slope'].std(), line_dash='dash', line_width=1
-        )
-        .add_hline(
-            y=-2 * beta_supply['short_slope'].std(), line_dash='dash', line_width=1
         )
     )
 
-    for _, infl in enumerate(infls, 1):
-        if (
-            beta_supply['short_slope'].iloc[infl]
-            < -1 * beta_supply['short_slope'].std()
-        ):
-            figs.add_vline(
-                x=beta_supply['short_slope'].index[infl + 1],
-                opacity=0.4,
-                line_color='green',
-            )
+    figs.data[0].name = 'Supply Rate (β > 1.9)'
+    figs.data[1].name = 'Dispersion (Q90 − Q10)'
 
     figs.update_traces(
-        texttemplate='%{y:.2f}',
-        textposition='top center',
         hovertemplate='Date: %{x}<br>Value: %{y:.2f}<br>',
-        marker_line_color='#800020',
-        marker_line_width=1.5,
     )
 
     figs.show()
